79581f3e14d05f1030b03a1d58c92afb0614f473
geneinmylife <epxjz@bristol.ac.uk>
2017-05-13 22:16:37 +0100

    add command line function into PhenoCorr.r

--- script/PhenoCorr.r
+++ script/PhenoCorr.r
@@ -1,7 +1,33 @@
-###estimate phenotypic correlation matrix using metaCCA###
-#rm(list=ls(all=TRUE))
-source("https://bioconductor.org/biocLite.R")
-biocLite("metaCCA")
+library(optparse)
 library(metaCCA)
-data <- read.table("S_XY_full_shin_metabolites.txt",header=T,row.names=1)
-S_YY_shin_metabolites = estimateSyy( S_XY = data)
+
+
+option_list = list(
+  make_option(c("-s", "--sumstats"), type="character", default=NULL, 
+              help="file with mulitple GWAS results", metavar="character"),
+
+  make_option(c("-o", "--out"), type="character", default="pheno.corr.txt", 
+              help="output of the phenotypic correlation estimation", metavar="character"),
+
+
+); 
+
+opt_parser = OptionParser(option_list=option_list);
+opt = parse_args(opt_parser);
+
+if (is.null(opt$sumstats)){
+  #print_help(opt_parser)
+  stop("Please provide the file name of the GWAS results. \n", call.=FALSE)
+}
+
+##example commond line
+#Rscript ./script/PhenoCorr.r --input ./data/PhenoSpD_input_example.txt --out example.pheno.corr.txt
+
+data <- read.table(opt$sumstats,header=T,row.names=1)
+
+##estimate phenotypic correlation matrix using metaCCA 
+S_YY = estimateSyy( S_XY = data)
+
+##write the phenotypic correlation matrix into the output file
+write.table(S_YY, file=opt$out, quote=F, sep="\t", row.names = F)
+
