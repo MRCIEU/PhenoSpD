38ffd5c06f115267e2de3ed4c2866034c354922a
geneinmylife <epxjz@bristol.ac.uk>
2017-07-21 22:47:23 +0100

    Delete DataPreparing.sh

--- script/DataPreparing.sh
+++
@@ -1,5 +0,0 @@
-###this is a script to reformat the raw GWAS summary statistics to a usable format for PhenoSpD###
-#download metabolites data from Shin et al
-wget http://mips.helmholtz-muenchen.de/proj/GWAS/gwas/gwas_server/shin_et_al.metal.out.tar.gz
-
-shin_et_al.metal.out.tar.gz
